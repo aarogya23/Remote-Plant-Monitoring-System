cd96e6f53a668aae063de0c2bfd79f2780cdcc76
Aarogya <aarogyathapa375@gmail.com>
2026-02-26 22:08:47 +0545

    Update on the spring boot application which will gather the data from hotsport and send the data to the database

---
+++ RemotePlantMonitoringSystem/RemotePlantMonitoringSystem/src/main/java/com/esp32/RemotePlantMonitoringSystem/RemotePlantMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.esp32.RemotePlantMonitoringSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotePlantMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotePlantMonitoringSystemApplication.class, args);
+	}
+
+}
